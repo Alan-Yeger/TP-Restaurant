551e6cec939cdaba36a26b498032ddf119a39d41
Alan-Yeger <101569295+Alan-Yeger@users.noreply.github.com>
2022-10-13 15:54:22 -0300

    holi

--- index.js
+++ index.js
@@ -28,10 +28,56 @@ app.get('/menu', (req, res) =>{
 //Ejercicio 2
 
 app.get('/menu:id', (req, res) =>{
-    id = parseint(req.body.params)
+    id = parseint(req.params.id)
     connection.query("SELECT * FROM menu WHERE id = (?)", [id], (err, rows) => {
 
         if (err) return res.status(406).json({ message: err})
         res.json(rows)
         })
     })
+
+//Ejercicio 3
+
+app.get('/principales', (req, res) =>{
+    id = parseint(req.params.id)
+    connection.query("SELECT * FROM menu WHERE tipo = principal", (err, rows) => {
+
+        if (err) return res.status(406).json({ message: err})
+        res.json(rows)
+        })
+    })
+
+//Ejercicio 4
+
+app.get('/postres', (req, res) =>{
+    id = parseint(req.params.id)
+    connection.query("SELECT * FROM menu WHERE tipo = postres", (err, rows) => {
+
+        if (err) return res.status(406).json({ message: err})
+        res.json(rows)
+        })
+    })
+
+//Ejercicio 5
+
+app.get('/bebidas', (req, res) =>{
+    id = parseint(req.params.id)
+    connection.query("SELECT * FROM menu WHERE tipo = bebida", (err, rows) => {
+
+        if (err) return res.status(406).json({ message: err})
+        res.json(rows)
+        })
+    })
+
+//Ejercicio 6
+
+app.post('/pedido', (req, res) =>{
+    let total = 0
+    const array = req.body.productos
+    for(let i = 0; i < array.length; i++){
+    connection.query("SELECT precio FROM menu WHERE id = ?", array[i].id)
+    console.log('hola')
+    //total += precioUnitario * array[i].cantidad
+
+    }
+})
